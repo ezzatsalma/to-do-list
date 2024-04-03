89982f48acd1d5e9b3b597ba8ed947edd259e09e
SALMA EZZAT <163161326+ezzatsalma@users.noreply.github.com>
2024-04-03 23:30:16 +0500

    programming

---
+++ index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let balance = 100000;
+let pincode = 46421;
+let pinnumber = await inquirer.prompt([{
+        name: "atmpin",
+        type: "number",
+        message: "enter your pin",
+    }]);
+if (pinnumber.atmpin === pincode) {
+    console.log("correct pin");
+    let pinanswer = await inquirer.prompt([{
+            name: "operation",
+            message: "select one",
+            type: "list",
+            choices: ["withdraw", "check balance"],
+        }]);
+    console.log(pinanswer);
+    if (pinanswer.operation === "withdraw") {
+        let amountans = await inquirer.prompt([{
+                name: "amount",
+                message: "enter amount",
+                type: "list",
+                choices: ["1000", "5000", "8000", "10000"]
+            }]);
+        console.log(`remaining amount:${balance - amountans.amount}`);
+    }
+    else {
+        console.log(`${balance}`);
+    }
+}
+else {
+    console.log("incorrect pin");
+}
